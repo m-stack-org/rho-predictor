01748071c96fb8a03c0eebe12e5e9422b021bb3e
Ksenia <ksenia.briling@epfl.ch>
2023-01-11 22:06:54 +0100

    Script to compute l-SOAP

---
+++ get_soap.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import equistore.io
+from lsoap import generate_lambda_soap_wrapper
+
+molfile = sys.argv[1]
+rascal_hypers = {
+    "cutoff": 4.0,
+    "max_radial": 8,  # Exclusive
+    "max_angular": 6,  # Inclusive
+    "atomic_gaussian_width": 0.3,
+    "radial_basis": {"Gto": {}},
+    "cutoff_function": {"ShiftedCosine": {"width": 0.5}},
+    "center_atom_weight": 1.0,
+}
+soap = generate_lambda_soap_wrapper([molfile], rascal_hypers, neighbor_species=[1,6,7,8])
+equistore.io.save(sys.argv[2], soap)
