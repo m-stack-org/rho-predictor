03d78cacd2cd144447b69e6380680fe7af75f7b7
Ksenia <ksenia.briling@epfl.ch>
2023-01-11 23:38:13 +0100

    Fix a bug related to different dir sorting

--- get_reference_ps.py
+++ get_reference_ps.py
@@ -20,8 +20,8 @@ def get_ref_idx(mollist, refs):
     mols = []
     idx_mol = []
     idx_atm = []
-    for i,file in enumerate(mollist):
-        mol = ase.io.read(file)
+    for i, file in enumerate(mollist):
+        mol = ase.io.read(file+'.xyz')
         n = mol.get_global_number_of_atoms()
         mols.append(mol)
         idx_mol += [i] * n
@@ -50,7 +50,7 @@ def prepare_blocks_dict(idx):
         mol_id, atom_id  = ref
         q = mols[mol_id][atom_id].number
         ref_q[iref] = q
-        tensor = equistore.io.load(npzlist[mol_id])
+        tensor = equistore.io.load(mollist[mol_id]+'.npz')
 
         for l in range(lmax+1):
             key = (l, q)
@@ -79,9 +79,10 @@ def prepare_blocks_dict(idx):
 
 if __name__=='__main__':
     lmax = 6
-    mollist = sorted(glob.glob("bfdb/dim*.xyz")) + sorted(glob.glob("bfdb/*mon*.xyz"))
-    npzlist = sorted(glob.glob("bfdb/dim*.npz")) + sorted(glob.glob("bfdb/*mon*.npz"))
-    refs = np.loadtxt('bfdb/refs_selection_1000.txt', dtype=int)
+
+    mydir = "bfdb/"
+    mollist = [mydir+f for f in np.loadtxt(mydir+"mollist.txt", dtype=str)]
+    refs = np.loadtxt(mydir+'refs_selection_1000.txt', dtype=int)
 
     mols, idx = get_ref_idx(mollist, refs)
     tensor, ref_q = prepare_blocks_dict(idx)
