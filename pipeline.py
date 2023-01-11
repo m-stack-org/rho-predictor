382c9346d345384674f1218735953057453efc5f
Ksenia <ksenia.briling@epfl.ch>
2023-01-11 23:37:38 +0100

    Normalize the kernels

--- pipeline.py
+++ pipeline.py
@@ -9,78 +9,30 @@ from lsoap import generate_lambda_soap_wrapper
 
 
 
-def kernel_nm(mol, soap, soap_ref, ref_q):
-    #(M, llmax, lmax, nel,
-    #el_dict, ref_elements,
-    #kernel_size, kernel_sparse_indices,
-    #power, power_ref,
-    #atom_counting, atomicindx,
-    #imol=None):
+def compute_kernel(mol, soap, soap_ref):
 
-    lmax = 6 # TODO
-
-    n = mol.get_global_number_of_atoms()
+    lmax   = max(np.array([list(x) for x in soap.keys])[:,0])
+    n      = mol.get_global_number_of_atoms()
     kernel = []
     for _ in range(n):
         kernel.append( {l: [] for l in range(lmax+1)})
-    print(kernel)
-
-
-    #k_NM = np.zeros(kernel_size, float)
 
     for iat, q in enumerate(mol.numbers):
-        print(q)
 
+        # Compute the kernel
         for l in range(lmax+1):
             block = soap.block(spherical_harmonics_l=l, species_center=q)
             isamp = block.samples.position((0, iat))
             vals  = block.values[isamp,:,:]
-
             block_ref = soap_ref.block(spherical_harmonics_l=l, species_center=q)
-            #irefs = np.where(ref_q==q)[0]
-            #for iref in irefs:
-            #    isamp_ref = block_ref.samples.position((iref,))
-            #    vals_ref  = block_ref.values[isamp_ref,:,:]
-            #    k = vals @ vals_ref.T
-            #    kernel[iat][l].append(k)
-            #kernel[iat][l] = np.array(kernel[iat][l])
             vals_ref  = block_ref.values
             kernel[iat][l] = np.einsum('rmx,Mx->rMm', vals_ref, vals) # maybe M and m should be transposed... TODO
 
-    for i in range(n):
-        for l in range(lmax+1):
-            print(kernel[i][l].shape)
-            print(kernel[i][l])
-            print()
+        # Normalize with zeta=2; should be in descendant order because l=0 is used
+        for l in range(lmax, -1, -1):
+            kernel[iat][l] = kernel[iat][l] * kernel[iat][0]
+    return kernel
 
-
-#    for q in ref_q:
-#        pass
-#    for iref in range(M):
-#        iel = ref_elements[iref]
-#        q   = el_dict[iel]
-#        for iatq in range(atom_counting[iel]):
-#            iat = atomicindx[iel,iatq]
-#            ik0 = kernel_sparse_indices[iref,iatq,0,0,0]
-#            for l in range(lmax[q]+1):
-#
-#                if imol is None:
-#                    powert = power[l][iat]
-#                else:
-#                    powert = power[l][imol,iat]
-#                powerr = power_ref[l][iref]
-#
-#                msize = 2*l+1
-#                if l==0:
-#                    ik = kernel_sparse_indices[iref,iatq,l,0,0]
-#                    k_NM[ik] = np.dot(powert,powerr)**zeta
-#                else:
-#                    kern = np.dot(powert,powerr.T) * k_NM[ik0]**(float(zeta-1)/zeta)
-#                    for im1 in range(msize):
-#                        for im2 in range(msize):
-#                            ik = kernel_sparse_indices[iref,iatq,l,im1,im2]
-#                            k_NM[ik] = kern[im2,im1]
-#    return k_NM
 
 
 
@@ -100,6 +52,7 @@ rascal_hypers = {
     "center_atom_weight": 1.0,
 }
 soap = generate_lambda_soap_wrapper(molfile, rascal_hypers, elements)
+
 
 
 # Load λ-SOAP for the reference environments
