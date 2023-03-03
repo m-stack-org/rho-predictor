ca188df50c358c13cfabd21def5942fbb3441e24
Ksenia <ksenia.briling@epfl.ch>
2023-03-03 19:02:23 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rho-predictor',
+    version='0.0.1',
+    description='Tool to predict the electronic density of molecules using a SA-GPR model',
+    url='https://github.com:lcmd-epfl/rho-predict.git',
+    install_requires=[],
+    packages=find_packages(),
+)
